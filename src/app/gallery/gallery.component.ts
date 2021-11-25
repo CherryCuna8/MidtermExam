a685e329afa7c61e7c531db89148670ffc8fda74
CherryCuna8 <you@example.com>
2021-11-25 11:50:28 +0800

    updating gallery content

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -19,6 +19,13 @@ export class GalleryComponent implements OnInit {
     {'id':5,'image':'../../assets/pic4.jpg'},
     {'id':6,'image':'../../assets/pic5.jpg'},
     {'id':7,'image':'../../assets/pic6.jpg'},
+    {'id':8,'image':'../../assets/pic1.jpg'},
+    {'id':9,'image':'../../assets/Cherry.jpg'},
+    {'id':10,'image':'../../assets/pic2.jpg'},
+    {'id':11,'image':'../../assets/pic3.jpg'},
+    {'id':12,'image':'../../assets/pic4.jpg'},
+    {'id':13,'image':'../../assets/pic5.jpg'},
+    {'id':14,'image':'../../assets/pic6.jpg'},
     
 
   ]
