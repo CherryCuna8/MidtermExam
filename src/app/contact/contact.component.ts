d0103a5e0cdf34eff0a85a0d44702fbc0191e9ce
CherryCuna8 <you@example.com>
2021-11-25 11:57:33 +0800

    updating contact content

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,6 +15,7 @@ export class ContactComponent implements OnInit {
     
     {'id':1,'name':'Facebook Account','description':'Username: Cherry G. Cuna','image':'../../assets/fb.png'},
     {'id':2,'name': 'Gmail Account','description':'Email:cherrycuna4@gmail.com','image':'../../assets/gmail.png'},
+    {'id':3,'name': 'Instagram Account','description':'Username:CherrGanda3817','image':'../../assets/insta.jfif'},
   ]
 
 }
